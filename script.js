b3022097e706b4129b556145ed21db613b46e8d0
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-03-27 18:10:19 -0400

    adding constants

--- script.js
+++ script.js
@@ -2,4 +2,19 @@
 // Don't let the shark catch up to the swimmer!
 // You have 3 attempts to guess the OCEAN ANIMAL!
 // CONSTANTS
-
+const imageOne = ;
+const imageTwo = ;
+const soundOne = ;
+const soundTwo = ;
+const oceanAnimals = new array [ 
+  "ORCA",
+  "DOLPHIN",
+  "JELLYFISH",
+  "CLOWNFISH",
+  "STARFISH",
+  "OCTOPUS",
+  "LOBSTER",
+  "CRAB",
+  "SQUID",
+  "PLANKTON",
+  "SEAHORSE"]
