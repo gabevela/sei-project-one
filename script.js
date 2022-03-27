8d478ce887d143dfc8d51ff1569d69d3f17e4ff8
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-03-27 18:07:46 -0400

    adding assets

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("alert");
+// SHARK ATTACK!!! Just like hangman, but better.
+// Don't let the shark catch up to the swimmer!
+// You have 3 attempts to guess the OCEAN ANIMAL!
+// CONSTANTS
+
