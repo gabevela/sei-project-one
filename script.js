23f95316f142963332baed12aacad68df38d6a87
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-04-02 12:01:04 -0400

    gv

--- script.js
+++ script.js
@@ -14,12 +14,12 @@
 //   "SQUID"]
 
   // APP's STATE VARIABLES
-let secretWord = "Dog";
+let secretWord = "DOLPHIN";
+console.log("the secret word is:" , secretWord);
 let lengthOfSecrectWord = secretWord.length;
-let guessedLetter = "";
 
 // CACHED DOM ELEMENTS 
-const letterBtnEl = document.getElementById("Aa");
+const letterBtnListEl = document.querySelectorAll("[data-letter]");
 const startGameBtnEl = document.getElementById("data-start-game");
 const endGameBtnEl = document.getElementById("data-end-game");
 
@@ -28,15 +28,21 @@ const scubaEl = document.getElementById("swimmer-status");
 const jawsEl = document.getElementById("jaws");
 
 // EVENT LISTENERS 
+letterBtnListEl.forEach(function(letterBtnEl){
+  letterBtnEl.addEventListener('click' , function (event){
+    console.log(event.target.innerHTML);
+    console.log("button clicked");
+  })
+});
 //startGameButtonEl.addEventListener('click' , "function");
-letterBtnEl.addEventListener('click' , function importGuessedLetter(){
-  guessedLetter = document.getElementById("Aa").innerHTML;
-  // document.getElementById("Aa").innerHTML = guessedLetter;
-  console.log("DOM KEYBOARD LETTER PRESSED");
-  console.log(guessedLetter);
-   // USE .textContent
-   return
- });
+// letterBtnEl.addEventListener('click' , function importGuessedLetter(){
+//   guessedLetter = document.getElementById("Aa").innerHTML;
+//   console.log("DOM KEYBOARD LETTER PRESSED");
+//   console.log(guessedLetter);
+//   onLetterPressDown();
+//   // PUSH METHOD NEEDED TO SAVE THE CORRECT LETTER TO THE ARRAY TO 
+//   //BUILD THE secretWord UP FROM SCRATCH ONCE THE PLAYER KEEPS PRESSING LETTERS
+//  });
 
 // let new Array = [
 // //insert  vlaues with a push method
@@ -45,14 +51,12 @@ letterBtnEl.addEventListener('click' , function importGuessedLetter(){
 // ]
 
 // FUNCTIONS
-function onLetterPressDown() {
+function onLetterPressDown(guessedLetter) {
   let indexOfFirst = secretWord.indexOf(guessedLetter);
   if (indexOfFirst >= 0) {
       console.log("CORRECT LETTER PRESSED!", guessedLetter, "is at index", indexOfFirst, "of the secret word.");
       document.getElementById(indexOfFirst).innerHTML = guessedLetter;
-
   } else {
       console.log("WRONG LETTER PRESSED")
   }
 }
-onLetterPressDown()
